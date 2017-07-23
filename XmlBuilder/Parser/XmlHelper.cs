e382025c9f88dbd07bb0e98acbec4220de3bddad
freeyxm <freeyxm@gmail.com>
2017-07-23 19:39:56 +0800

    Update XmlHelper.

--- XmlBuilder/Parser/XmlHelper.cs
+++ XmlBuilder/Parser/XmlHelper.cs
@@ -1,15 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Xml;
 
 namespace XmlBuilder.Parser
 {
     class XmlHelper
     {
+        #region XmlNode
         public static int ParseInt(XmlNode node)
         {
-            return int.Parse(node.InnerText);
+            return ParseInt(node.InnerText);
         }
+
+        public static float ParseFloat(XmlNode node)
+        {
+            return ParseFloat(node.InnerText);
+        }
+
+        public static bool ParseBool(XmlNode node)
+        {
+            return ParseBool(node.InnerText);
+        }
+
+        public static string ParseString(XmlNode node)
+        {
+            return node.InnerText;
+        }
+        #endregion
+
+        #region XmlAttribute
+        public static int ParseInt(XmlAttribute attr)
+        {
+            return ParseInt(attr.Value);
+        }
+
+        public static float ParseFloat(XmlAttribute attr)
+        {
+            return ParseFloat(attr.Value);
+        }
+
+        public static bool ParseBool(XmlAttribute attr)
+        {
+            return ParseBool(attr.Value);
+        }
+
+        public static string ParseString(XmlAttribute attr)
+        {
+            return attr.Value;
+        }
+        #endregion
+
+        #region Parse
+        public static int ParseInt(string value)
+        {
+            return int.Parse(value);
+        }
+
+        public static float ParseFloat(string value)
+        {
+            return float.Parse(value);
+        }
+
+        public static bool ParseBool(string value)
+        {
+            return bool.Parse(value);
+        }
+        #endregion
     }
 }
